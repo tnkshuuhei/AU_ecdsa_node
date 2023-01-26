06c98be8cd225158e00f3f6299f510e4bf19ef6e
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-01-26 12:21:59 +0900

    finish week1

--- server/index.js
+++ server/index.js
@@ -7,12 +7,14 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "044a486ac7fe08fe250cd9e6eccf4309ba90fcaa29faad90a17734cff1a430a190c7589bf823d63b2511b69a95fa94ecb1e0f32891fbbf0622652eb7e7a495b743": 100,
+  "04f0bf98f64911c577241ad567bf581d3e91a4e2397758d629f3bdfeebac827a1ef56efbb23e90eca69e4332067fc88ad1b6a1873bc690d8015dadb43e61f5ed5b": 50,
+  "04de96392eba925ab014ed53b71e7d21e0a1d58aada513472db8a4ee7edeabf5a4e4324e006a20536ff34b2c83167606b28412dfa751d1d36f04daa05a934376ef": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
+  // TODO: get a signature from the client-side application
+  // recover the public address from the signature
   const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
